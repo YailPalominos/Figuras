0cf418ffc9956eea5f44ca315855fa056ae1fad0
izel-f <valeitzel43@gmail.com>
2024-09-12 13:27:34 -0600

    Create Esfera.py

---
+++ Esfera.py
@@ -0,0 +1,15 @@
+import math
+
+class esfera:
+    def __init__(self):
+        self.radio = float (input ('Ingresa el radio: '))
+
+
+    def mostrar_area():
+        area = 4*(math.pi*radio^[2])
+        print ('Valor de area: ' + (area))
+
+# Crear una instancia de la clase Piramide y ejecutar el cálculo
+if __name__ == "__main__":
+    esfera = esfera()
+    esfera.mostrar_area()
