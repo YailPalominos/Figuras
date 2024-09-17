c92311623f620c3ecb2507ad788e7ac28451fe3e
K5ope6 <161170018+K5ope6@users.noreply.github.com>
2024-09-16 19:08:12 -0600

    Tetraedro.py

--- Tetraedro.py
+++ Tetraedro.py
@@ -1,11 +1,16 @@
-import math
-
+#Kevin Jesús Martin López Doblado
+#Función matemática
+import math 
+#Método que calcula la superficie de un tetraedro con el valor de la arista
 def areaTetraedo(arista):
+    #Formula para el calculo
     resultado = math.sqrt(3)*(arista ** 2) 
     return resultado
-
+#Variable para capturar la longitud de la arista del tetraedro
 longitudArista =float(input("Ingrese la longitud de la arista: "))
+#Llamada a la función areaTetraedro
 uts = areaTetraedo(longitudArista)
 
+#Imprime el área de la superficie del Tetraedro
 print(f"El área de la superficie del tetraedro es: {uts}")
 input("Presiona Enter para salir")
